e2212ee36ecca44361401e36cc3ae8b6dc8ea81a
Dias Sagatov <101350324+diassagatov@users.noreply.github.com>
2024-07-20 22:42:00 +0500

    PWA is not available

--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
 const CACHE_NAME = 'slot-simulator-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/project.js',
-  '/manifest.json',
-  '/sound/deposit.wav',
-  '/sound/reel-click.wav',
-  // Add other assets you want to cache
+  '/Slot_simulator/',
+  '/Slot_simulator/index.html',
+  '/Slot_simulator/style.css',
+  '/Slot_simulator/project.js',
+  '/Slot_simulator/manifest.json',
+  '/Slot_simulator/sound/deposit.wav',
+  '/Slot_simulator/sound/reel-click.wav',
+  '/Slot_simulator/icons/icon-192x192.png',
+  '/Slot_simulator/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
